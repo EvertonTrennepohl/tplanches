580fd1b2ca07ffe679f7217e425f7e5e30ba4137
EvertonTrennepohl <gauchotp@gmail.com>
2022-02-21 16:07:12 -0300

    First commit

---
+++ src/main/java/com/trennepohl/tplanches/TplanchesApplication.java
@@ -0,0 +1,13 @@
+package com.trennepohl.tplanches;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TplanchesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TplanchesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/trennepohl/tplanches/TplanchesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.trennepohl.tplanches;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TplanchesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
